f6739ee60195f6e873835fc1108a13e3fe00abba
Gage Thornberry <gagedoug13@gmail.com>
2019-08-21 04:11:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(array)
     puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  new_array = []
+  planeteer_calls.map {|calls|}
 end
 
 def long_planeteer_calls# code an argument here
