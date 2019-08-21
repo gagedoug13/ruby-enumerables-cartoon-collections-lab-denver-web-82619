8ef2e0299e1d974926a5043aed362c7646bb8bee
Gage Thornberry <gagedoug13@gmail.com>
2019-08-21 04:14:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ end
 def summon_captain_planet(planeteer_calls)
   new_array = []
   planeteer_calls.each {|calls|}
-  new_array.push(`#{calls.capitalize}!`)
-  p new_array
+  new_array<<(`#{calls.capitalize}!`)
+
 end
 
 def long_planeteer_calls# code an argument here
